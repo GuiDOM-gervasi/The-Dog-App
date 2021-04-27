969fb2326cf529cccb9e4e84bdb93fcb1cd8d796
Guido <guidogervasi11@gmail.com>
2021-04-27 18:38:55 -0300

    format documents

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,56 +1,57 @@
-import axios from 'axios'
+import axios from "axios";
 
-  export function addDogs(dog) {
-    return function(dispatch) {
-      return axios.post('http://localhost:3001/dog',dog)
-        .then(response => {
-          console.log(response.data)
-          dispatch({ type: "ADD_DOG", payload: response.data });
-        })
-        .catch(err=>{
-          console.log(err)
-        });
-    };
-  }
-
-  export function getDogDetails(id) {
-    return function(dispatch) {
-      return axios.get(`http://localhost:3001/dogs/${id}`)
-        .then(response => {
-          dispatch({ type: "GET_DOGDETAILS", payload: response.data });
-        });
-    };
-  }
-
-export function getDogsByName(name) {
-    return function(dispatch) {
-      return axios.get(`http://localhost:3001/dogs?name=${name}`)
-        .then((response) => {
-          dispatch({ type: "GET_NAMEDOGS", payload: response.data });
-        })
-        .catch(err=>{
-          console.log(err)
-        });
-    };
-  }
-
-export function setFilter(filter){
-  console.log("entre")
-  return {
-    type: "SET_FILTER",
-    payload: filter
-  }
+export function addDogs(dog) {
+  return function (dispatch) {
+    return axios
+      .post("http://localhost:3001/dog", dog)
+      .then((response) => {
+        console.log(response.data);
+        dispatch({ type: "ADD_DOG", payload: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 }
 
+export function getDogDetails(id) {
+  return function (dispatch) {
+    return axios.get(`http://localhost:3001/dogs/${id}`).then((response) => {
+      dispatch({ type: "GET_DOGDETAILS", payload: response.data });
+    });
+  };
+}
+
+export function getDogsByName(name) {
+  return function (dispatch) {
+    return axios
+      .get(`http://localhost:3001/dogs?name=${name}`)
+      .then((response) => {
+        dispatch({ type: "GET_NAMEDOGS", payload: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
+
+export function setFilter(filter) {
+  console.log("entre");
+  return {
+    type: "SET_FILTER",
+    payload: filter,
+  };
+}
 
 export function getTemperaments() {
-    return function(dispatch) {
-      return axios.get('http://localhost:3001/temperament')
-        .then(response => {
-          dispatch({ type: "GET_TEMPES", payload: response.data });
-        })
-        .catch(err=>{
-          console.log(err)
-        });
-    };
-  }
+  return function (dispatch) {
+    return axios
+      .get("http://localhost:3001/temperament")
+      .then((response) => {
+        dispatch({ type: "GET_TEMPES", payload: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,42 +1,41 @@
 const initialState = {
-    dogsName:[],
-    newDogs:[],
-    dogDetails: {},
-    tempes: []
-  };
+  dogsName: [],
+  newDogs: [],
+  dogDetails: {},
+  tempes: [],
+};
 
-const rootReducer = (state=initialState, action) => {
-    switch(action.type) {
-        case "ADD_DOG":
-            return {
-                ...state,
-                newDogs: state.newDogs.concat(action.payload), 
-            };
-        case "GET_DOGDETAILS":
-        return {
-                ...state,
-                dogDetails: action.payload     
-        }
-        case "GET_NAMEDOGS":
-        return {
-            ...state,
-            dogsName: action.payload
-        }
-        case "GET_TEMPES":
-        return {
-            ...state,
-            tempes: state.tempes.concat(action.payload), 
-        }
-        case "SET_FILTER":
-            console.log("entre")
-            return {
-                ...state,
-                dogsName: action.payload 
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_DOG":
+      return {
+        ...state,
+        newDogs: state.newDogs.concat(action.payload),
+      };
+    case "GET_DOGDETAILS":
+      return {
+        ...state,
+        dogDetails: action.payload,
+      };
+    case "GET_NAMEDOGS":
+      return {
+        ...state,
+        dogsName: action.payload,
+      };
+    case "GET_TEMPES":
+      return {
+        ...state,
+        tempes: state.tempes.concat(action.payload),
+      };
+    case "SET_FILTER":
+      console.log("entre");
+      return {
+        ...state,
+        dogsName: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-            }
-        default:
-            return state
-    }
-}
-
-export default rootReducer
+export default rootReducer;
